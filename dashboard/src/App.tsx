e01fa13f990fb46754fadfe5a4776f0c01daaedb
jrdzha <jared.zhao@polyture.com>
2022-04-23 19:08:05 -0700

    add react project

---
+++ dashboard/src/App.tsx
@@ -0,0 +1,15 @@
+import Home from './pages/Home';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Home/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
